27c524fa8b1ad7dde664ab98472a4a59e5152a21
najoan125 <tiaaa0012@gmail.com>
2025-07-11 01:21:18 +0900

    Remove ActionbarListener and introduce player_stats.yml for stat configuration
    - Deleted `ActionbarListener` class from the `actionbar` module.
    - Added `player_stats.yml` for defining maximum stat points and player stat attributes.
    - Enhanced `ZeroActionbar` to log activation on initialization.

--- src/main/java/kr/hyfata/zero/modules/actionbar/ZeroActionbar.kt
+++ src/main/java/kr/hyfata/zero/modules/actionbar/ZeroActionbar.kt
@@ -3,6 +3,10 @@ package kr.hyfata.zero.modules.actionbar
 import org.bukkit.plugin.java.JavaPlugin
 
 class ZeroActionbar(var plugin: JavaPlugin) {
+    init {
+        plugin.logger.info("Zero Actionbar has been enabled.")
+    }
+
     fun onDisable() {
         plugin.logger.info("Zero Actionbar has been disabled.")
     }
