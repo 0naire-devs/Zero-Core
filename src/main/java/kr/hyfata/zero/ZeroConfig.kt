8496d5662d652474d7faa185b58448367daa1e2a
najoan125 <tiaaa0012@gmail.com>
2025-07-20 21:25:26 +0900

    1.3.0 beta

--- src/main/java/kr/hyfata/zero/ZeroConfig.kt
+++ src/main/java/kr/hyfata/zero/ZeroConfig.kt
@@ -1,6 +1,7 @@
 package kr.hyfata.zero
 
 import kr.hyfata.zero.config.AbstractConfig
+import kr.hyfata.zero.helper.stats.player.PlayerStatsConfig
 import kr.hyfata.zero.modules.mailbox.MailboxConfig
 import kr.hyfata.zero.modules.scoreboard.ScoreboardConfig
 import org.bukkit.plugin.java.JavaPlugin
@@ -8,14 +9,17 @@ import org.bukkit.plugin.java.JavaPlugin
 class ZeroConfig(plugin: JavaPlugin) {
     var scoreboardConfig: AbstractConfig = ScoreboardConfig()
     var mailboxConfig: AbstractConfig = MailboxConfig()
+    var playerStatsConfig: AbstractConfig = PlayerStatsConfig()
 
     init {
         scoreboardConfig.init(plugin)
         mailboxConfig.init(plugin)
+        playerStatsConfig.init(plugin)
     }
 
     fun save() {
         scoreboardConfig.saveConfig()
         mailboxConfig.saveConfig()
+        playerStatsConfig.saveConfig()
     }
 }
